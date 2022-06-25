09ba7209175c3438f303912fbbd65a20297ba3da
mohaam20 <76072798+mohaam20@users.noreply.github.com>
2022-06-25 13:44:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,42 @@ const todolist = document.querySelector(".todo-list")
 const allertms = document.querySelector(".alert")
 const kid = todobutton.parentNode
 
+
 todobutton.addEventListener("click",addTodo)
 todolist.addEventListener("click",women,true)
+
+console.log(localStorage.getItem("todos"))
 
 function women(event){
     let btnnn = event.target
     let panel = btnnn.closest("li")
-    if (btnnn.className != "show")
-    panel.remove()
+    console.log(btnnn.className)
+    if (btnnn.className == "checkLogo" && panel.className == "solid" ){
+        console.log("arrived")
+    panel.classList.remove("solid")    
+    panel.classList.add("glass")}
+    
+    else if( btnnn.className === "dellLogo"){
+        function bob(gestt){
+            gestt.remove()
+        }
+        panel.classList.add("falll")
+        setTimeout(bob,1000,panel)
+        console.log("arrived 2")
+    }
+    else if(btnnn.className== "checkLogo" && panel.className == "glass"){
+      panel.classList.remove("glass")
+      panel.classList.add("solid")
+      console.log("through")      
+   }
+
+
 }
 
-function nice(target){
-    target.style.backgroundColor = "yellow"
-    var big = target.parentNode
-    big.remove()
-}
+
 
 function mate(frshed){
-    frshed.style.backgroundColor = "rgb(77, 233, 233)";
+    frshed.style.backgroundColor = "#B25068";
 }
 
 function addTodo(event){
@@ -31,18 +49,24 @@ function addTodo(event){
     const check = document.createElement("button")
     const field = todoinput.value ;
     const kids = kid.children.length
-    
+    const dell = document.createElement("button")
+
     setTimeout(mate,200,tsContainer)
 
     if(field !== ""){
         
         task.innerHTML = field
         check.innerHTML = '<i class="fa-solid fa-check"></i>'
-        check.classList.add("pop")
+        dell.innerHTML='<i class="fa-solid fa-trash"></i>'
+        check.classList.add("checkLogo")
+       
+        dell.classList.add("dellLogo")
+        tsContainer.classList.add("solid")
         tsContainer.appendChild(task)
+        saveLocal(field)
+        tsContainer.append(dell)
         tsContainer.append(check)
         todolist.appendChild(tsContainer);
-
         if(kids >= 3){
             allertms.style.left = "50%"
         }
@@ -53,11 +77,55 @@ function addTodo(event){
     }
     todoinput.value = ""
      
-    tsContainer.classList.toggle("show")
 
 }
 
+function sleep(){
 
+}
+
+function saveLocal(todo){
+
+
+let todos = JSON.parse(localStorage.getItem('todos')) ?? []
+    
+
+
+todos.push(todo);
+
+
+console.log(todos)
+
+
+
+localStorage.setItem('todos',JSON.stringify(todos));
+
+}
+
+function readLocal(sleeping){
+    const tsContainer = document.createElement("li")
+    const task = document.createElement("p")
+    const check = document.createElement("button")
+    const field = sleeping ;
+    const kids = kid.children.length
+    const dell = document.createElement("button")
+
+    setTimeout(mate,200,tsContainer)
+
+    task.innerHTML = field
+    check.innerHTML = '<i class="fa-solid fa-check"></i>'
+    dell.innerHTML='<i class="fa-solid fa-trash"></i>'
+    check.classList.add("checkLogo")
+    
+    dell.classList.add("dellLogo")
+    tsContainer.classList.add("solid")
+    tsContainer.appendChild(task)
+    saveLocal(field)
+    tsContainer.append(dell)
+    tsContainer.append(check)
+    todolist.appendChild(tsContainer);
+    
+}
 
 
 
